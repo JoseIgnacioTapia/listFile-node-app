c4b119f29eea4958a8620d38044dfb41507a5d8a
José Ignacio <josignaciot@gmail.com>
2020-10-17 19:55:57 -0300

    different methods to solve the function

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util');
+
+// Method #2
+// const lstat = util. promisify(fs.lstat);
   
-fs.readdir(process.cwd(), (err, filenames) => {
+// Method #3
+// const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
   // EITHER
   // err === an error object, wich means something went wrong 
   // OR
@@ -13,5 +20,30 @@ fs.readdir(process.cwd(), (err, filenames) => {
     console.log(err);
   }
 
-  console.log(filenames);
+  for(let filename of filenames) {
+    try {
+      const stats = await lstat(filename);
+
+      console.log(filename, stats.isFile());
+    } catch(err) {
+      console.log(err);
+    }
+    
+  }
 });
+
+// Method #1
+const lstat = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.lstat(filename, (err, stats) => {
+      if(err) {
+        reject(err);
+      }
+
+      resolve(stats);
+    });
+  });
+};
+
+
+
