9cb88b2186032036eda845e8a2fc72aad7b143de
José Ignacio <josignaciot@gmail.com>
2020-10-18 17:59:03 -0300

    Promise.all-Based Solution

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const util = require('util');
 // const lstat = util. promisify(fs.lstat);
   
 // Method #3
-// const { lstat } = fs.promises;
+const { lstat } = fs.promises;
 
 fs.readdir(process.cwd(), async (err, filenames) => {
   // EITHER
@@ -20,30 +20,31 @@ fs.readdir(process.cwd(), async (err, filenames) => {
     console.log(err);
   }
 
-  for(let filename of filenames) {
-    try {
-      const stats = await lstat(filename);
+  const statPromises = filenames.map(filename => {
+    return lstat(filename);
+  });
+  
+  const allStats = await Promise.all(statPromises);
 
-      console.log(filename, stats.isFile());
-    } catch(err) {
-      console.log(err);
-    }
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
     
+    console.log(filenames[index], stats.isFile());
   }
 });
 
 // Method #1
-const lstat = (filename) => {
-  return new Promise((resolve, reject) => {
-    fs.lstat(filename, (err, stats) => {
-      if(err) {
-        reject(err);
-      }
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if(err) {
+//         reject(err);
+//       }
 
-      resolve(stats);
-    });
-  });
-};
+//       resolve(stats);
+//     });
+//   });
+// };
 
 
 
